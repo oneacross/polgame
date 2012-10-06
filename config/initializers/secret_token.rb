41169386701fdae9ae987e757d58af71c65e4bc5
Matt Mendell <matthew.mendell@gmail.com>
2012-10-06 19:16:06 -0400

    Remove rails crud 3!

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Polgame::Application.config.secret_token = '06dec7e727e9fba8f653aede67fad92b066adc7a7ab5aa5da78dd9454c1b50f16014ad849e57e0e796516f5ab07ca81e8c46610cdc1dca7a687a109431a180e8'
