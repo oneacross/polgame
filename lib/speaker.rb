f79fffb6fd5db7c83a50e793d5c2621e2762373e
Matt Mendell <matthew.mendell@gmail.com>
2012-10-09 15:09:03 -0400

    Simplify the Speaker class

--- lib/speaker.rb
+++ lib/speaker.rb
@@ -1,46 +1,31 @@
 class Speaker
 
-    attr_accessor :wapo_id
+  def self.speakers()
+    [
+      {"wapo_api" => 3,"first_name" => "Chris","last_name"=>"Christie","party"=>"Republican","url"=>"http://www.state.nj.us/governor/library/photos/gov_christie.jpg","width"=>170,"height"=>242},
+      {"wapo_api" => 5,"first_name" => "Clint","last_name"=>"Eastwood","party"=>"Republican","url"=>"http://www.newsmax.com/Newsmax/files/1a/1a81ded4-e973-42ee-9079-85da4547dada.jpg","width"=>170,"height"=>242},
+      {"wapo_api" => 7,"first_name" => "Jim","last_name"=>"Lehrer","party"=>"","url"=>"http://www.pbs.org/newshour/aboutus/images/photo_bio_lehrer.jpg","width"=>170,"height"=>242},
+      {"wapo_api" => 2,"first_name" => "Mitt","last_name"=>"Romney","party"=>"Republican","url"=>"http://reason.com/assets/mc/mriggs/MittRomneyProfilePic.jpg","width"=>521,"height"=>648},
+      {"wapo_api" => 6,"first_name" => "Bill","last_name"=>"Clinton","party"=>"Democrat","url"=>"http://www.florencedailynews.com/wp-content/uploads/2012/09/bill-clinton-picture.jpg","width"=>170,"height"=>242},
+      {"wapo_api" => 1,"first_name" => "Barack","last_name"=>"Obama","party"=>"Democrat","url"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Official_portrait_of_Barack_Obama.jpg/220px-Official_portrait_of_Barack_Obama.jpg","width"=>220,"height"=>299},
+      {"wapo_api" => 4,"first_name" => "Paul","last_name"=>"Ryan","party"=>"Republican","url"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Paul_Ryan_official_portrait.jpg/220px-Paul_Ryan_official_portrait.jpg","width"=>170,"height"=>242}
+    ]
+  end
 
-    def initialize(wapo_rsp)
-      @wapo_id = wapo_rsp['id']
-      @first_name = wapo_rsp['first_name']
-      @last_name = wapo_rsp['last_name']
-      @party = wapo_rsp['party']
-      @photo = {}
-    end
+  # API
+  def self.get_speaker(speaker_id)
+    speaker = speakers().select do |spkr|
+      spkr['wapo_api'] == speaker_id.to_i
+    end.first
 
-    def add_photo(url, width, height)
-      @photo = {
-        :url => url,
-        :width => width,
-        :height => height
-      }
-    end
+    speaker
+  end
 
-    def to_json()
-      return {
-        :wapo_api => @wapo_id,
-        :first_name => @first_name,
-        :last_name => @last_name,
-        :party => @party
-      }.merge(@photo).to_json
-    end
+  def self.get_wrong_speaker(speaker_id)
+    speaker = speakers().select do |spkr|
+      spkr['wapo_api'] != speaker_id.to_i
+    end.sample
 
-    # API
-    def self.get_speaker(speakers, speaker_id)
-      speaker = speakers.select do |spkr|
-        spkr['wapo_api'] == speaker_id.to_i
-      end.first
-
-      speaker
-    end
-
-    def self.get_wrong_speaker(speakers, speaker_id)
-      speaker = speakers.select do |spkr|
-        spkr['wapo_api'] != speaker_id.to_i
-      end.sample
-
-      speaker
-    end
+    speaker
+  end
 end
--- polgame.rb
+++ polgame.rb
@@ -8,18 +8,7 @@ require 'redis'
 require "httparty"
 require 'speaker'
 
-NUMBER_OF_QUOTES = 100
-
-# Hack!
-speakers_db = [
-  {"wapo_api" => 3,"first_name" => "Chris","last_name"=>"Christie","party"=>"Republican","url"=>"http://www.state.nj.us/governor/library/photos/gov_christie.jpg","width"=>170,"height"=>242},
-  {"wapo_api" => 5,"first_name" => "Clint","last_name"=>"Eastwood","party"=>"Republican","url"=>"http://www.newsmax.com/Newsmax/files/1a/1a81ded4-e973-42ee-9079-85da4547dada.jpg","width"=>170,"height"=>242},
-  {"wapo_api" => 7,"first_name" => "Jim","last_name"=>"Lehrer","party"=>"","url"=>"http://www.pbs.org/newshour/aboutus/images/photo_bio_lehrer.jpg","width"=>170,"height"=>242},
-  {"wapo_api" => 2,"first_name" => "Mitt","last_name"=>"Romney","party"=>"Republican","url"=>"http://reason.com/assets/mc/mriggs/MittRomneyProfilePic.jpg","width"=>521,"height"=>648},
-  {"wapo_api" => 6,"first_name" => "Bill","last_name"=>"Clinton","party"=>"Democrat","url"=>"http://www.florencedailynews.com/wp-content/uploads/2012/09/bill-clinton-picture.jpg","width"=>170,"height"=>242},
-  {"wapo_api" => 1,"first_name" => "Barack","last_name"=>"Obama","party"=>"Democrat","url"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Official_portrait_of_Barack_Obama.jpg/220px-Official_portrait_of_Barack_Obama.jpg","width"=>220,"height"=>299},
-  {"wapo_api" => 4,"first_name" => "Paul","last_name"=>"Ryan","party"=>"Republican","url"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Paul_Ryan_official_portrait.jpg/220px-Paul_Ryan_official_portrait.jpg","width"=>170,"height"=>242}
-]
+NUMBER_OF_QUOTES = 10
 
 cached_results = []
 
@@ -42,9 +31,9 @@ get '/game.json' do
     game_quote['text'] = "#{game_quote['text'][0, 300]} ..."
   end
 
-  correct_speaker = Speaker.get_speaker(speakers_db, game_quote['speaker']['id'])
+  correct_speaker = Speaker.get_speaker(game_quote['speaker']['id'])
   correct_speaker['correct'] = true
-  wrong_speaker = Speaker.get_wrong_speaker(speakers_db, game_quote['speaker']['id'])
+  wrong_speaker = Speaker.get_wrong_speaker(game_quote['speaker']['id'])
   left_speaker, right_speaker = [correct_speaker, wrong_speaker].shuffle()
 
   { :quote => game_quote,
